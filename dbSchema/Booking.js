e4ef2c4919a3bf940188330be4bd529675f4ca14
Ankit Kumar <ankit_sony3@hotmail.com>
2017-09-15 02:24:33 +0530

    created Bookng Schema

---
+++ dbSchema/Booking.js
@@ -0,0 +1,26 @@
+var mongoose = require('mongoose');
+
+var bookingSchema = new mongoose.Schema({
+    restaurantId: { type: mongoose.Schema.Types.ObjectId, ref: 'Restaurant' },
+    tableId: { type: mongoose.Types.ObjectId },
+    bookedOn: { type: Date, default: Date.now },
+    startTime: Date,
+    endTime: Date,
+
+    bookingStatus: { type: String, enum: ['CONFIRMED', 'CANCELLED', 'BLOCKED'] },
+
+    // Users details
+    name: String,
+    email: String,
+    phone: Number,
+
+    // store userId 
+    // INFO: skip it for now
+    // userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User' }
+
+});
+
+bookingSchema.index({ restaurantId: true });
+bookingSchema.index({ restaurantId: 1, tableId: 1 });
+
+module.exports = mongoose.model('Booking', bookingSchema);
