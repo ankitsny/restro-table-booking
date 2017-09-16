3c8291f12020be8b38226b96b987da9e1ab4c428
Ankit Kumar <ankit_sony3@hotmail.com>
2017-09-17 03:23:03 +0530

    updated to es6

--- routes/review.routes.js
+++ routes/review.routes.js
@@ -1,15 +1,15 @@
-var router = require('express').Router();
-var reviewController = require('../controllers/review.controller');
+const router = require('express').Router();
+const reviewController = require('../controllers/review.controller');
 
 // INFO: use middlewares for authentication
 
 router
-    .route('/')
-    .get(reviewController.getReviews)
-    .post(reviewController.postReview)
-    .put(reviewController.putReview)
-    .patch(reviewController.patchReview)
-    .delete(reviewController.deleteReview);
+  .route('/')
+  .get(reviewController.getReviews)
+  .post(reviewController.postReview)
+  .put(reviewController.putReview)
+  .patch(reviewController.patchReview)
+  .delete(reviewController.deleteReview);
 
 
 module.exports = router;
